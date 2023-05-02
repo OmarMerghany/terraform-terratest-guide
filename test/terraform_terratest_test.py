901fc530544cbf0520599747d80bc0eb7c19c0f7
OmarMerghany <omar.merghany@starsandstories.com>
2023-05-03 01:35:06 +0200

    update the test to use subprocess

--- test/terraform_terratest_test.py
+++ test/terraform_terratest_test.py
@@ -1,49 +1,45 @@
 import os
+import subprocess
 import time
-
-from terratest import options
-from terratest import terraform
-from terratest import test
-from terratest.http_helper import http_get
-
-
-def setup_environment():
-    # Set the Google Cloud project to use for this test
-    os.environ["GOOGLE_CLOUD_PROJECT"] = os.environ.get("GCP_PROJECT_ID")
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-
+import requests
 
 def test_cloud_run_app():
     # Set up the Terraform options with the path to the Terraform code and the values to use
-    setup_environment()
+    os.environ["GOOGLE_CLOUD_PROJECT"] = os.environ.get("GCP_PROJECT_ID")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
     terraform_dir = os.path.join(os.getcwd(), "terraform")
     test_name = "test-" + os.environ["GITHUB_RUN_NUMBER"]
-    options = terraform.Options(
-        terraform_dir=terraform_dir,
-        vars={
-            "project_id": os.environ.get("GCP_PROJECT_ID"),
-            "region": "europe-west4",
-            "service_name": test_name,
-            "image": "gcr.io/google-samples/hello-app:1.0",
-        },
-    )
+    options = [
+        "-auto-approve",
+        "-var",
+        f"project_id={os.environ.get('GCP_PROJECT_ID')}",
+        "-var",
+        "region=europe-west4",
+        "-var",
+        f"service_name={test_name}",
+        "-var",
+        "image=gcr.io/google-samples/hello-app:1.0",
+    ]
 
     # Deploy the Cloud Run application using Terraform
-    terraform.apply(options)
+    subprocess.run(["terraform", "init", terraform_dir], check=True)
+    subprocess.run(["terraform", "apply", terraform_dir] + options, check=True)
 
     # Wait for the Cloud Run service to start up
     time.sleep(30)
 
     # Send an HTTP GET request to the Cloud Run service
     expected_response = "Hello World!"
-    url = terraform.output(options, "url")
-    response = http_get(url, expected_status=200, expected_response=expected_response)
+    url = subprocess.run(["terraform", "output", "url"], capture_output=True, text=True).stdout.strip()
+    response = requests.get(url)
+    response_text = response.text.strip()
 
     # Check that the response matches the expected response
-    test.assertEqual(expected_response, response, f"Unexpected response: {response}")
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+    assert response_text == expected_response, f"Unexpected response: {response_text}"
 
     # Destroy the resources using Terraform
-    terraform.destroy(options)
+    subprocess.run(["terraform", "destroy", "-auto-approve", terraform_dir] + options, check=True)
 
 
 if __name__ == "__main__":
